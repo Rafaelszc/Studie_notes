662853676c79aa3e8131a974a1f7a71d9d41cd51
Rafaelszc <rafaelbjj84@gmail.com>
2024-11-08 02:30:45 -0300

    add: selection sort with java

---
+++ src/sort_algorithms/selection_sort.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Algorithm{
+
+    static void print_array(int[] array){
+        int len = array.length;
+
+        for (int i=0; i<len; i++){
+            System.out.printf("%d", array[i]);
+
+            if (i < len-1){
+                System.out.print(", ");
+            }
+            else {
+                System.out.print("\n");
+            }
+        }
+    }
+
+    static int[] selection_sort(int[] array){
+        int len = array.length;
+
+        for (int i=0; i<len-1; i++){
+            int min = i;
+
+            for (int j=i+1; j<len; j++){
+                if (array[min] > array[j]){
+                    min = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args){
+        int array[] = {213, 2, 54, 12, 53, 2, 75, 4, 674, 24};
+
+        print_array(array);
+
+        array = selection_sort(array);
+
+        print_array(array);
+    }
+}
